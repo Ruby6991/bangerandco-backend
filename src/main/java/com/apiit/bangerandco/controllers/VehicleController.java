dc5878d2eda124b8be99db2da0327141896c572a
rabiya faizal <rabiyaf1@gmail.com>
2020-05-15 03:53:12 +0530

    implemented basic vehicle management

---
+++ src/main/java/com/apiit/bangerandco/controllers/VehicleController.java
@@ -0,0 +1,52 @@
+package com.apiit.bangerandco.controllers;
+
+import com.apiit.bangerandco.dtos.UserDTO;
+import com.apiit.bangerandco.dtos.VehicleDTO;
+import com.apiit.bangerandco.models.User;
+import com.apiit.bangerandco.models.Vehicle;
+import com.apiit.bangerandco.services.UserService;
+import com.apiit.bangerandco.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class VehicleController {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @PostMapping("/AddVehicle")
+    public ResponseEntity<Boolean> AddVehicle(@RequestBody Vehicle vehicle){
+        return vehicleService.addVehicle(vehicle);
+    }
+
+    @PostMapping("/GetVehicleModel")
+    public ResponseEntity<List<VehicleDTO>> GetVehicleByModel(@RequestBody Vehicle vehicle){
+        return vehicleService.getVehicleByModel(vehicle.getModel());
+    }
+
+    @PostMapping("/GetVehicleLicense")
+    public ResponseEntity<VehicleDTO> getVehicleByLicense(@RequestBody Vehicle vehicle){
+        return vehicleService.getVehicleByLicenseNo(vehicle.getLicenseNo());
+    }
+
+    @PostMapping("/GetVehicleList")
+    public ResponseEntity<List<VehicleDTO>> getVehicleList(){
+        return vehicleService.getVehicleList();
+    }
+
+    @DeleteMapping("/DeleteVehicle/{id}")
+    public ResponseEntity<Boolean> DeleteVehicle(@PathVariable int id){
+        return vehicleService.deleteVehicle(id);
+    }
+
+    @PutMapping("/UpdateVehicle/{id}")
+    public ResponseEntity<Vehicle> UpdateVehicle(@PathVariable int id, @RequestBody Vehicle vehicle){
+        return vehicleService.updateVehicle(id,vehicle);
+    }
+
+}
---
+++ src/main/java/com/apiit/bangerandco/services/VehicleService.java
@@ -0,0 +1,88 @@
+package com.apiit.bangerandco.services;
+
+import com.apiit.bangerandco.dtos.UserDTO;
+import com.apiit.bangerandco.dtos.VehicleDTO;
+import com.apiit.bangerandco.enums.CustomerState;
+import com.apiit.bangerandco.enums.UserType;
+import com.apiit.bangerandco.models.User;
+import com.apiit.bangerandco.models.Vehicle;
+import com.apiit.bangerandco.repositories.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VehicleService {
+
+    @Autowired
+    VehicleRepository vehicleRepo;
+
+    @Autowired
+    ModelToDTO modelToDTO;
+
+    public ResponseEntity<Boolean> addVehicle(Vehicle newVehicle){
+            vehicleRepo.save(newVehicle);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<VehicleDTO>> getVehicleByModel(String model){
+        List<VehicleDTO> vehicleDTOS = new ArrayList<>();
+        Iterable<Vehicle> vehicleList = vehicleRepo.findAll();
+        for(Vehicle vehicle : vehicleList){
+            if(vehicle.getModel().equals(model)){
+                vehicleDTOS.add(modelToDTO.vehicleToDTO(vehicle));
+            }
+        }
+        return new ResponseEntity<>(vehicleDTOS, HttpStatus.OK);
+    }
+
+    public ResponseEntity<VehicleDTO> getVehicleByLicenseNo(String licenseNo){
+        Iterable<Vehicle> vehicleList = vehicleRepo.findAll();
+        for(Vehicle vehicle : vehicleList){
+            if(vehicle.getLicenseNo()==licenseNo){
+                return new ResponseEntity<>(modelToDTO.vehicleToDTO(vehicle), HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<Boolean> deleteVehicle(int id){
+        try{
+            vehicleRepo.deleteById(id);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(false, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Vehicle> updateVehicle(int id, Vehicle newVehicle){
+        Optional<Vehicle> vehicleOptional = vehicleRepo.findById(id);
+        if(vehicleOptional.isPresent()){
+            Vehicle vehicle = vehicleOptional.get();
+            vehicle.setDescription(newVehicle.getDescription());
+            vehicle.setImgUrl(newVehicle.getImgUrl());
+            vehicle.setMileage(newVehicle.getMileage());
+            vehicle.setQuantity(newVehicle.getQuantity());
+            vehicle.setRates(newVehicle.getRates());
+            vehicle.setServiceDate(newVehicle.getServiceDate());
+            vehicleRepo.save(vehicle);
+            return new ResponseEntity<>(vehicle,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<List<VehicleDTO>> getVehicleList() {
+        List<VehicleDTO> vehicleDTOList = new ArrayList<>();
+        Iterable<Vehicle> vehicleList = vehicleRepo.findAll();
+        for(Vehicle vehicle : vehicleList){
+            vehicleDTOList.add(modelToDTO.vehicleToDTO(vehicle));
+        }
+        return new ResponseEntity<>(vehicleDTOList, HttpStatus.OK);
+    }
+}
